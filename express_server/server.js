9318b0776d5333e7c340ac415d4de57ed26dc177
darceymckelvey <darceymckelvey@gmail.com>
2026-06-10 17:24:17 -0300

    get post put delete

--- express_server/server.js
+++ express_server/server.js
@@ -4,9 +4,29 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
+});
+
+app.get('/api/v1/cars', (req, res) => {
+    res.send('Cars endpoint');
+});
+
+app.post('/api/v1/cars', (req, res) => {
+    res.send('Create a new car');
+});
+
+app.put('/api/v1/cars/:id', (req, res) => {
+    res.send(`Update car with id ${req.params.id}`);
+});
+
+app.delete('/api/v1/cars/:id', (req, res) => {
+    res.send(`Delete car with id ${req.params.id}`);
+});
+
+app.get('/api/v1/cars/:id', (req, res) => {
+    res.send(`Get car with id ${req.params.id}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
